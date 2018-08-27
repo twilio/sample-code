7248cff7555ce72813c3523f12db9c76a8fd736d
David Prothero <david@prothero.com>
2018-08-27 11:34:11 -0700

    SMS Quickstart code sample

---
+++ api/v2010/message/create-quickstart/create-quickstart.3.x.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+         body: `Join Earth's mightest heroes. Like Kevin Bacon.`,
+         from: '+15017122661',
+         to: '+15558675310'
+       })
+      .then(message => console.log(message.sid))
+      .done();
---
+++ api/v2010/message/create-quickstart/create-quickstart.5.x.rb
@@ -0,0 +1,17 @@
+# Download the helper library from https://www.twilio.com/docs/ruby/install
+require 'rubygems'
+require 'twilio-ruby'
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+message = @client.messages
+                 .create(
+                    body: "Join Earth's mightest heroes. Like Kevin Bacon.",
+                    from: '+15017122661',
+                    to: '+15558675310'
+                  )
+
+puts message.sid
