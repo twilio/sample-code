b0caacd0182dbad9b5fffce751770a026e7f8039
Paul Kamp <pkamp3@users.noreply.github.com>
2018-08-29 17:11:35 -0700

    Update create-send-sms.5.x.php

--- api/v2010/message/create-send-sms/create-send-sms.5.x.php
+++ api/v2010/message/create-send-sms/create-send-sms.5.x.php
@@ -1,9 +1,5 @@
 <?php
-
-// Update the path below to your autoload.php,
-// see https://getcomposer.org/doc/01-basic-usage.md
-require_once '/path/to/vendor/autoload.php';
-
+require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
 
 // Find your Account Sid and Auth Token at twilio.com/console
