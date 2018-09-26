4535899a0b5507d7d1a0603b845991f17f995db7
Kat King <kking@twilio.com>
2018-09-26 11:32:22 -0400

    Newly generated samples for new Verify domain

---
+++ verify/v1/verification_check/create-default/create-default.5.x.rb
@@ -0,0 +1,15 @@
+# Download the helper library from https://www.twilio.com/docs/ruby/install
+require 'rubygems'
+require 'twilio-ruby'
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+verification_check = @client.verify
+                            .services('VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+                            .verification_checks
+                            .create(code: 'code')
+
+puts verification_check.sid
