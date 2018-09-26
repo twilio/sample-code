e8906d8ae283364b356207618d77f2c18848096f
Kat King <kking@twilio.com>
2018-09-26 15:57:31 -0400

    Auto-generate code snippets for new Voice resources

---
+++ voice/v1/bulk_country_update/create-default/create-default.3.x.js
@@ -0,0 +1,11 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.voice.voicePermissions
+  .bulkCountryUpdates
+  .create({updateRequest: 'updateRequest'})
+  .then(bulk_country_update => console.log(bulk_country_update.updateCount))
+  .done();
