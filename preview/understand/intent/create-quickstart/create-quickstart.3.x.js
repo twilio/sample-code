3d9823ec7bce9c325aee2a10ea51ceb6b62c3c74
Kat King <kking@twilio.com>
2018-09-26 13:28:07 -0400

    Add custom samples for Understsand docs

---
+++ preview/understand/intent/create-quickstart/create-quickstart.3.x.js
@@ -0,0 +1,12 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.preview.understand
+              .assistants('UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+              .intents
+              .create({friendlyName: 'tell a joke', uniqueName: 'tell-a-joke'})
+              .then(intent => console.log(intent.sid))
+              .done();
---
+++ preview/understand/model_build/create-quickstart/create-quickstart.3.x.js
@@ -0,0 +1,12 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.preview.understand
+              .assistants('UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+              .modelBuilds
+              .create({uniqueName: 'v0.1'})
+              .then(model_build => console.log(model_build.sid))
+              .done();
