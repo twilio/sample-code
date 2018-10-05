6a35c6ac3377e750d078f856c186d65723f121e6
Kat King <kking@twilio.com>
2018-10-05 15:40:53 -0400

    Add generated samples for pausing current call/conf recording

---
+++ api/v2010/recording/update-example-pause-current-call-recording/update-example-pause-current-call-recording.5.x.php
@@ -0,0 +1,20 @@
+<?php
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once '/path/to/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Find your Account Sid and Auth Token at twilio.com/console
+$sid    = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token  = "your_auth_token";
+$twilio = new Client($sid, $token);
+
+$recording = $twilio->calls("CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                    ->recordings("Twilio.CURRENT")
+                    ->update("paused", // status
+                             array("pauseBehavior" => "pauseBehavior")
+                    );
+
+print($recording->callSid);
