31bf88a53aac5737724967fb591dedc788ae44e5
Kat King <kking@twilio.com>
2018-10-15 13:43:54 -0700

    Add custom sample to show updating defaults

---
+++ autopilot/v1/defaults/update-example-1/update-example-1.5.x.php
@@ -0,0 +1,26 @@
+<?php
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once '/path/to/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Find your Account Sid and Auth Token at twilio.com/console
+$sid    = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token  = "your_auth_token";
+$twilio = new Client($sid, $token);
+
+$defaults = $twilio->autopilot->v1->assistants("UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                                  ->defaults()
+                                  ->update(array(
+                                               "defaults" => array(
+                                                   "defaults" => array(
+                                                       "assistant_initiation" => "task://hello-world",
+                                                       "fallback" => "task://hello-world"
+                                                   )
+                                               )
+                                           )
+                                  );
+
+print($defaults->assistantSid);
