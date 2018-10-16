f7693c0bce12d0948c027a4c9364b866eaaa29ba
Kat King <kking@twilio.com>
2018-10-15 17:28:21 -0700

    Add generated codes samples for creating a task with actions

---
+++ autopilot/v1/task/update-example-1/update-example-1.7.x.java
@@ -0,0 +1,35 @@
+// Install the Java helper library from twilio.com/docs/java/install
+
+import com.twilio.Twilio;
+import com.twilio.rest.autopilot.v1.assistant.Task;
+
+import java.util.HashMap;
+
+public class Example {
+    // Find your Account Sid and Token at twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Task task = Task.updater(
+                "UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "UDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+            .setActions(
+                new HashMap<String, Object>()
+                {{
+                    put("actions", new String [] {
+                        new HashMap<String, Object>()
+                        {{
+                            put("say", new HashMap<String, Object>()
+                            {{
+                                put("speech", "I was going to look for my missing watch, but I could never find the time.");
+                            }});
+                        }}
+                    });
+                }})
+            .update();
+
+        System.out.println(task.getFriendlyName());
+    }
+}
