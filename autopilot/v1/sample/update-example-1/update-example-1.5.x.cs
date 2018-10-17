f61fcaadb35bec4a6512f1d63c365de78271e677
Kat King <kking@twilio.com>
2018-10-17 00:55:19 -0700

    Add custom snippet to show updating to en-US'

---
+++ autopilot/v1/sample/update-example-1/update-example-1.5.x.cs
@@ -0,0 +1,27 @@
+// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using Twilio;
+using Twilio.Rest.Autopilot.V1.Assistant.Task;
+
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Token at twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var sample = SampleResource.Update(
+            language: "en-US",
+            pathAssistantSid: "UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            pathTaskSid: "UDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            pathSid: "UFXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        );
+
+        Console.WriteLine(sample.DateCreated);
+    }
+}
