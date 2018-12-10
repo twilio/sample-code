c1109e9db2786bb7f4e8295cd0980aca1e4ad371
David Prothero <david@prothero.com>
2018-12-10 15:10:40 -0800

    Proxy reserved number example

---
+++ proxy/v1/phone_number/create-add-reserved-phone-number/create-add-reserved-phone-number.3.x.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.proxy.services('KSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+            .phoneNumbers
+            .create({
+               sid: 'PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+               isReserved: true
+             })
+            .then(phone_number => console.log(phone_number.sid))
+            .done();
---
+++ proxy/v1/phone_number/create-add-reserved-phone-number/create-add-reserved-phone-number.5.x.php
@@ -0,0 +1,22 @@
+<?php
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once '/path/to/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Find your Account Sid and Auth Token at twilio.com/console
+$sid    = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token  = "your_auth_token";
+$twilio = new Client($sid, $token);
+
+$phone_number = $twilio->proxy->v1->services("KSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                                  ->phoneNumbers
+                                  ->create(array(
+                                               "sid" => "PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                                               "isReserved" => True
+                                           )
+                                  );
+
+print($phone_number->sid);
