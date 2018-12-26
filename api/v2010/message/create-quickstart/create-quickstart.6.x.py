5dc9f6cc66e1a2a8a2fb17c4a71929cc41177c92
chanzformerz <35658439+chanzformerz@users.noreply.github.com>
2018-12-26 11:18:48 +1100

    Update - TwilioAccount

--- api/v2010/message/create-quickstart/create-quickstart.6.x.py
+++ api/v2010/message/create-quickstart/create-quickstart.6.x.py
@@ -3,15 +3,15 @@ from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'your_auth_token'
+account_sid = 'ACee1482b81269ae1bab0a0313af59f914'
+auth_token = '6c7a846b153f3829b0e50fa4c5861ceb'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+15017122661',
-                     to='+15558675310'
+                     body="Testing 1,2,3",
+                     from_='+61488846562',
+                     to='+61488824269'
                  )
 
 print(message.sid)
