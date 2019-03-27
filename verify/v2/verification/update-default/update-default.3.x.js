13bfd6822bd2ed4beb2bc15aba1b798e59bac13a
Kelley Robinson <krobinson@twilio.com>
2019-03-27 15:34:10 -0400

    Add PSD2 samples, remove PSD2 fields from default samples

---
+++ verify/v2/verification/update-default/update-default.3.x.js
@@ -0,0 +1,11 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.verify.services('VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+             .verifications('sid')
+             .update({status: 'canceled'})
+             .then(verification => console.log(verification.to));
---
+++ verify/v2/verification_check/create-default/create-default.6.x.py
@@ -0,0 +1,16 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+verification_check = client.verify \
+                           .services('VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+                           .verification_checks \
+                           .create(to='+14159373912', code='1234')
+
+print(verification_check.sid)
